b14878d4abba514a9d162f6b1de4358e97ae2063
Tsehla <petrusnk@gmail.com>
2018-03-15 04:42:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//reusables
+// bakground image
+function background_image(_image, id){
+    document.getElementById(id).style.backgroundImage= "url('/images/"+_image+".png')";
+    
+}
+
+//slide
+var start=1;
+function slide(){
+   background_image(start, "header");
+    if(start==3){
+        start=0;
+    }
+start=start+1;
+}
+setInterval(slide, 3000);
